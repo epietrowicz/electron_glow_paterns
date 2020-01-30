379cbc481e348c3fd6dc3dc878ca7dbe564a95d2
Eric Pietrowicz <pietrowicz.eric@gmail.com>
2020-01-30 16:59:32 -0600

    color picker is working

--- window.js
+++ window.js
@@ -17,16 +17,16 @@ function getDatasets() {
         1,
       ],
       backgroundColor: [
-        'rgba(0, 0, 255, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(0, 255, 0, 1)',
-        'rgba(0, 0, 255, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(0, 255, 0, 1)',
-        'rgba(0, 0, 255, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(0, 255, 0, 1)',
-        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)',
       ],
       borderWidth: 1,
       borderColor: '#777',
@@ -75,18 +75,61 @@ function drawChart() {
         let element = this.getElementAtEvent(event);
         if (element.length > 0) {
             var label = element[0]._model.label;
+            console.log(label);
             drawPrompt(label);
         }
       }
     },
   });
-  setInterval(updateDatasets, 1000);
+        //setInterval(updateDatasets(led, colorString), 1000);
 }
+/*
 var i = 0;
 function updateDatasets(){
   i++;
-  chart.data.datasets[i].backgroundColor = 'rgba(0, 255, 0, 1)'
+  chart.data.datasets[0].backgroundColor[i] = 'rgba(0, 255, 0, 1)'
+  if(i > 10){
+    i = 0;
+  }
   chart.update();
+}
+*/
+
+function updateDatasets(led, colorString){
+
+  chart.data.datasets[0].backgroundColor[led] = colorString
+  led++;
+  if(led > 10){
+    led = 0;
+  }
+  chart.update();
+}
+
+function getLEDLocation(label){
+  switch(label){
+    case 'LED 1':
+      return 0;
+    case 'LED 2':
+      return 1;
+    case 'LED 3':
+      return 2;
+    case 'LED 4':
+      return 3;
+    case 'LED 5':
+      return 4;
+    case 'LED 6':
+      return 5;
+    case 'LED 7':
+      return 6;
+    case 'LED 8':
+      return 7;
+    case 'LED 9':
+      return 8;
+    case 'LED 10':
+      return 9;
+    default:
+      return 0;
+  }
 }
 
 function drawPrompt(label){
@@ -139,16 +182,14 @@ function drawPrompt(label){
   })
   .then((result) => {
     if (result) {
-      console.log('obtained result', result),
-      
-      //IN CASE OF MULTI-INPUT THE RESULT WILL BE IN THIS FORMAT:
-      elem = inputArray[1];
-      {
-        console.log(elem)
-      }
-      
+      console.log('obtained result', result);
+      var colorString = 'rgba(' + result.Red + ', ' + result.Blue + ', ' + result.Green + ', 1)';
+      console.log(colorString);
+      var led = getLEDLocation(label);
+      console.log(led);
+      updateDatasets(led, colorString);
     } else {
-      // in this case the window has been closed or the input are null
+      //TODO: add indicator that you need all values
     }
   })
   .catch((error) => {
